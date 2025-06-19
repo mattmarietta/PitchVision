5b618d89e83e729ccc124a0fba112047c70cc25e
mattmarietta <matt.marietta24@gmail.com>
2025-06-18 19:26:43 -0700

    Updated Code and tracking with colors

---
+++ camera_movement/__init__.py
@@ -0,0 +1,1 @@
+from .cam_movement import CameraMovement
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2
+import numpy as np
+from team_assigner import TeamAssigner
+from player_ball_assignment import PlayerBallAssigner
+from camera_movement import CameraMovement
+from view_transformer import ViewTransformer
+from speed_and_distance import SpeedAndDistance
 
 def main():
     #Read the video
@@ -13,11 +20,70 @@ def main():
                                               stub_path="stubs/track_stubs.pkl")
 
     #Draw output vid
-    ## Draw object tracks
+    ## Draw object object_tracks
     #print(f"Number of video frames: {len(video_frames)}")
-    #print(f"Number of tracked frames: {len(object_tracks['players'])}")
+    #print(f"Number of tracked frames: {len(object_object_tracks['players'])}")
 
-    output_video_frames = tracker.draw_circles(video_frames, object_tracks)
+# Get object positions 
+    tracker.add_position_to_tracks(object_tracks)
+
+    # camera movement estimator
+    camera_movement_estimator = CameraMovement(video_frames[0])
+    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,
+                                                                                read_from_stub=True,
+                                                                                stub_path='stubs/camera_movement_stub.pkl')
+    camera_movement_estimator.add_adjust_positions_to_tracks(object_tracks,camera_movement_per_frame)
+
+
+    # View Trasnformer
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(object_tracks)
+
+    # Interpolate Ball Positions
+    object_tracks["ball"] = tracker.interpolate_ball_positions(object_tracks["ball"])
+
+    # Speed and distance estimator
+    speed_and_distance_estimator = SpeedAndDistance()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(object_tracks)
+
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    object_tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(object_tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            object_tracks['players'][frame_num][player_id]['team'] = team 
+            object_tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    
+    # Assign Ball Aquisition
+    player_assigner =PlayerBallAssigner()
+    team_ball_control= []
+    for frame_num, player_track in enumerate(object_tracks['players']):
+        ball_bbox = object_tracks['ball'][frame_num][1]['bbox']
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            object_tracks['players'][frame_num][assigned_player]['has_ball'] = True
+            team_ball_control.append(object_tracks['players'][frame_num][assigned_player]['team'])
+        else:
+            team_ball_control.append(team_ball_control[-1] if team_ball_control else 1)
+    team_ball_control= np.array(team_ball_control)
+
+
+    #Draw output 
+    #Draw object Tracks
+    output_video_frames = tracker.draw_annotations(video_frames, object_tracks,team_ball_control)
+
+    #Draw Camera movement
+    output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
+
+    #Draw Speed and Distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames,object_tracks)
 
     #Save video
     save_video(output_video_frames, 'output_videos/output_video.avi')
